6aaabad6c56f4d8e782779643265142de260b834
flotisable <s09930698@gmail.com>
2023-02-08 22:24:05 +0800

    update: GlazeWM

--- Sources/glazewm/tools/chocolateyinstall.ps1
+++ Sources/glazewm/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = $env:ChocolateyPackageName
-$url            = 'https://github.com/lars-berger/GlazeWM/releases/download/v1.11.0/GlazeWM_x86_1.11.exe'
-$url64          = 'https://github.com/lars-berger/GlazeWM/releases/download/v1.11.0/GlazeWM_x64_1.11.exe'
-$checksum       = '9B84DEBBFFF7AFE452E00F09D8413B9B6E1F518E214400191C7FA2D6BEB881B2'
+$url            = 'https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x86_1.11.1.exe'
+$url64          = 'https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x64_1.11.1.exe'
+$checksum       = 'c47d0f53228441b3e010456ae87644eded80daa72a1a2a16b2d96fedc7225246'
 $checksumType   = 'sha256'
-$checksum64     = 'C348925508F52130EDB200C6FFD06D4532FF73CDF9312B4B7821FCB4457622A3'
+$checksum64     = 'ddf137ad32f608b3d400a2d204f958145e15878844d01702e6a10093b503b959'
 $checksumType64 = 'sha256'
 
 Get-ChocolateyWebFile $packageName "$toolsDir/${packageName}.exe" $url $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
