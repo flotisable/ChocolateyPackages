310de8bc4062d4f51a4a28ed46ee3465d4fab082
flotisable <s09930698@gmail.com>
2023-09-19 04:42:12 -0700

    update: spec for all packages

--- Sources/efm-langserver/tools/chocolateyinstall.ps1
+++ Sources/efm-langserver/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = $env:ChocolateyPackageName
-$url            = 'https://github.com/mattn/efm-langserver/releases/download/v0.0.44/efm-langserver_v0.0.44_windows_amd64.zip'
-$checksum       = '1A4ACBF25ED14F89A32EB17B1B0B9F0B6E92B47C799475B818913C8AACA22AA2'
+$url            = 'https://github.com/mattn/efm-langserver/releases/download/v0.0.48/efm-langserver_v0.0.48_windows_amd64.zip'
+$checksum       = 'a54860023879da7194f3567ccb240009f20638d8a9fbe0434d6f95439c5aa3d1'
 $checksumType   = 'sha256'
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir -checksum $checksum -checksumType $checksumType
--- Sources/glazewm/tools/chocolateyinstall.ps1
+++ Sources/glazewm/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageName    = $env:ChocolateyPackageName
-$url            = 'https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x86_1.11.1.exe'
-$url64          = 'https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x64_1.11.1.exe'
-$checksum       = 'c47d0f53228441b3e010456ae87644eded80daa72a1a2a16b2d96fedc7225246'
+$url            = 'https://github.com/glazerdesktop/GlazeWM/releases/download/v2.0.1/GlazeWM_x86_2.0.1.exe'
+$url64          = 'https://github.com/glazerdesktop/GlazeWM/releases/download/v2.0.1/GlazeWM_x64_2.0.1.exe'
+$checksum       = '5ae0b93c4c98e7a6d66deb753bf210b64cf539099558f5a1b0aa40278f7fc6c9'
 $checksumType   = 'sha256'
-$checksum64     = 'ddf137ad32f608b3d400a2d204f958145e15878844d01702e6a10093b503b959'
+$checksum64     = '0212458d22a9c698493589221706a35a8e72927ff57c3f2475e991aad49a662d'
 $checksumType64 = 'sha256'
 
 Get-ChocolateyWebFile $packageName "$toolsDir/${packageName}.exe" $url $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
